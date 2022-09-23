1d026712b25263b900bdfae4135ad32102c0b8d6
Gert <Gert@DESKTOP-0K4GCSE>
2022-09-23 20:30:39 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using TextTyper;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		if (args.Length == 0)
+		{
+			Console.WriteLine("Provide the string to type as an argument");
+			Console.WriteLine("Press a key to quit...");
+			Console.Read();
+			System.Environment.Exit(11);
+		}
+		else
+		{
+			Console.WriteLine("Put the focus on the field to type and press the ScrollLock key to write your text");
+			KeyboardListener.Run(args[0]);
+		}
+	}
+}
